963789a5b7d5ae0fca6fe87857afd1ce61686a17
root <subhradeepray2017@gmail.com>
2023-01-14 15:16:21 +0530

    Adding Project

---
+++ src/main/java/com/subhradeep/SpringBootReactApp/SpringBootReactAppApplication.java
@@ -0,0 +1,12 @@
+package com.subhradeep.SpringBootReactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/subhradeep/SpringBootReactApp/SpringBootReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhradeep.SpringBootReactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
