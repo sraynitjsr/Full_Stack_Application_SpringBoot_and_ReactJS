52f0a4813532d36c7e5e3299c1a78f8c3ce03046
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-02 00:43:57 +0530

    Full Stack Application Using Java SpringBoot and ReactJS

--- com/sray/main/MyMain.java
+++
@@ -1,7 +0,0 @@
-package com.sray.main;
-
-public class MyMain {
-    public static void main(String[] args) {
-        System.out.println("Full Stack Application Using Spring Boot and ReactJS");
-    }
-}
