aa470add5871386e7e1cb81a6a01c040a9b79b86
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-02 01:07:52 +0530

    Movies Full Stack REST API CRUD Application With ReactJS

---
+++ movies/src/test/java/com/sraynitjsr/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sraynitjsr.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
