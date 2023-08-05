efb58ced60afdfac91a246c21744d0b0c29529da
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-05 23:01:22 +0530

    Movies POJO

--- movies/src/main/java/com/sraynitjsr/movies/Movies.java
+++ movies/src/main/java/com/sraynitjsr/movies/Movies.java
@@ -1,1 +1,14 @@
- 
+package com.sraynitjsr.movies;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "movies")
+public class Movies {
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private List<String> moviesList;
+}
